0502b3f05478ec67acfc10bbc6394dbbf91facff
skyscraper <kwikius@yahoo.com>
2013-02-18 08:58:36 +0000

    uav rc_pulse - initial commit

---
+++ quan/uav/rc_pulse.hpp
@@ -0,0 +1,16 @@
+#ifndef QUAN_UAV_RC_PULSE_HPP_INCLUDED
+#define QUAN_UAV_RC_PULSE_HPP_INCLUDED
+
+#include <quan/time.hpp>
+
+namespace quan { namespace uav {
+   struct rc_pulse{
+
+      static constexpr auto num_channels = 10U;
+      typedef quan::time_<int16_t>::us us;
+      us channel[num_channels];
+
+   };
+}}//quan::uav
+
+#endif // QUAN_UAV_RC_PULSE_HPP_INCLUDED
