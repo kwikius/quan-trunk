36030a18133e582f27b284f146ff7744770f9729
skyscraper <kwikius@yahoo.com>
2013-03-09 10:30:17 +0000

    added template params for LengthType and AngleType. Should probabably rename this as gps_position

--- quan/uav/position.hpp
+++ quan/uav/position.hpp
@@ -3,19 +3,35 @@
 
 #include <quan/length.hpp>
 #include <quan/angle.hpp>
+#include <quan/implicit_cast.hpp>
 
 namespace quan { namespace uav {
 
+   template <typename AngleType, typename LengthType>
 	struct position {
+       typedef LengthType length_type;
+       typedef AngleType angle_type;
        position():lat{0},lon{0},alt{0}{}
 		 position(
-         quan::angle_<int32_t>::deg10e7 const & lat_in, 
-         quan::angle_<int32_t>::deg10e7 const & lon_in,
-         quan::length::m const & h_in )
+         angle_type const & lat_in, 
+         angle_type const & lon_in,
+         length_type const & h_in )
 	    :lat{lat_in},lon{lon_in},alt{h_in} {}
-		 quan::angle_<int32_t>::deg10e7 lat;
-		 quan::angle_<int32_t>::deg10e7 lon;
-		 quan::length_<int32_t>::mm     alt;
+		 angle_type    lat;
+		 angle_type    lon;
+		 length_type   alt;
+       template <typename LengthType1, typename AngleType1>
+       position & operator = (position<LengthType1,AngleType1> const & in)
+       {
+         this->lat = in.lat;
+         this->lon = in.lon;
+         this->alt = in.alt;
+       }
+       template <typename LengthType1, typename AngleType1>
+       position (position<LengthType1,AngleType1> const & in)
+       : lat{quan::implicit_cast<angle_type>(in.lat)},
+         lon{quan::implicit_cast<angle_type>(in.lon)},
+         alt{quan::implicit_cast<length_type>(in.alt)}{}
 	};
 
 }}
