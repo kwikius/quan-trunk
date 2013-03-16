475baffc419dde7c4db419c4d322ea4257a6f7b1
skyscraper <kwikius@yahoo.com>
2013-03-16 09:52:01 +0000

    add licence info

--- quan/config.hpp
+++ quan/config.hpp
@@ -1,5 +1,23 @@
 #ifndef QUAN_CONFIG_HPP_INCLUDED
 #define QUAN_CONFIG_HPP_INCLUDED
+/*
+ Copyright (c) 2005-2013 Andy Little 
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+// See QUAN_ROOT/quan_matters/index.html for documentation.
+
 #if (defined _MSC_VER) && (_MSC_VER >= 1200)
 #  pragma once
 #endif
@@ -11,9 +29,6 @@
 #if defined __AVR__ && defined abs
 #undef abs
 #endif
-
-// Copyright Andrew Little 2005-2006
-// See QUAN_ROOT/quan_matters/index.html for documentation.
 
 /*
     Various Configuration options for Quan
--- quan/meta/low_bits_mask.hpp
+++ quan/meta/low_bits_mask.hpp
@@ -1,5 +1,21 @@
 #ifndef QUAN_META_LOW_BITS_MASK_HPP_INCLUDED
 #define QUAN_META_LOW_BITS_MASK_HPP_INCLUDED
+/*
+ Copyright (c) 2006-2013 Andy Little 
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 
 #include <cstdint>
 #include <quan/bit.hpp>
@@ -17,6 +33,5 @@ namespace quan{ namespace meta{
      };
 
 }} // quan::meta
-
 
 #endif // QUAN_META_LOW_BITS_MASK_HPP_INCLUDED
