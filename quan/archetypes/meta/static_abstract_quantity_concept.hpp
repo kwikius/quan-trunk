457d2ed6e1c5375d07320f11f50f5a52b86d0a7c
skyscraper <kwikius@yahoo.com>
2013-07-18 14:07:33 +0100

    add license info

--- quan/archetypes/meta/static_abstract_quantity_concept.hpp
+++ quan/archetypes/meta/static_abstract_quantity_concept.hpp
@@ -1,10 +1,22 @@
 #ifndef QUAN_META_STATIC_ABSTRACT_QUANTITY_CONCEPT_HPP_INCLUDED
 #define QUAN_META_STATIC_ABSTRACT_QUANTITY_CONCEPT_HPP_INCLUDED
 
-// Copyright Andrew Little 2005
+/*
+ Copyright (c) 2005-2013 Andy Little 
 
-// See <QUAN_ROOT/quan_matters/index.html> for documentation.
-
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 /*
     associated metafunctions for the StaticAbstractQuantity Concept.
     These are required to be specialised per model.
