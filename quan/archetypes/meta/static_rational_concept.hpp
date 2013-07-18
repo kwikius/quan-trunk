17aab34a403a011c710aedd214895451eb1a4e6e
skyscraper <kwikius@yahoo.com>
2013-07-18 14:09:49 +0100

    mod include guard and add license

--- quan/archetypes/meta/static_rational_concept.hpp
+++ quan/archetypes/meta/static_rational_concept.hpp
@@ -1,17 +1,25 @@
-#ifndef QUAN_META_RATIONAL_CONCEPT_HPP_INCLUDED
-#define QUAN_META_RATIONAL_CONCEPT_HPP_INCLUDED
+#ifndef QUAN_ARCHETYPES_META_RATIONAL_HPP_INCLUDED
+#define QUAN_ARCHETYPES_META_RATIONAL_HPP_INCLUDED
 #if (defined _MSC_VER) && (_MSC_VER >= 1200)
 #  pragma once
 #endif
 
-// Copyright Andrew Little 2005
-//
- 
- 
- 
-//
-// See QUAN_ROOT/quan_matters/index.html for documentation.
+/*
+ Copyright (c) 2005-2013 Andy Little 
 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 /*
     StaticRational extends StaticNumeric
 */
