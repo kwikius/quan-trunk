16df8444edcf66272afbf2d1620362c071606efb
skyscraper <kwikius@yahoo.com>
2013-11-17 15:31:12 +0000

    start on stm32f0 and generic stm32 stuff

---
+++ quan/stm32/f0/periph_map.hpp
@@ -0,0 +1,16 @@
+#ifndef QUAN_STM32_F0_PERIPH_MAP_HPP_INCLUDED
+#define QUAN_STM32_F0_PERIPH_MAP_HPP_INCLUDED
+
+namespace quan{ namespace stm32f0{
+
+   struct periph_map{
+      static constexpr uint32_t gpiof = 0x48001400;
+      static constexpr uint32_t gpiod = 0x48000C00; 
+      static constexpr uint32_t gpioc = 0x48000800 ;
+      static cosntexpr uint32_t gpiob = 0x48000400;                       
+
+   };
+}} 
+
+
+#endif // QUAN_STM32_F0_PERIPH_MAP_HPP_INCLUDED
