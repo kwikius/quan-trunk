458d319746207accd325a4d68644aaeb838e6d19
skyscraper <kwikius@yahoo.com>
2013-11-17 15:25:59 +0000

    get working on avr

--- quan/meta/uint32.hpp
+++ quan/meta/uint32.hpp
@@ -6,13 +6,19 @@
 // See QUAN_ROOT/quan_matters/index.html for documentation.
 
 #include <quan/config.hpp>
+#ifndef __AVR__
 #include <type_traits>
 #include <cstdint>
+#else
+#include <stdint.h>
+#include <quan/std/tr1/integral_constant.hpp>
+#endif
 
 namespace quan{namespace meta{
 
    template <uint32_t N>
    struct uint32 : std::integral_constant<uint32_t,N>{};
+
 }}
 
 #endif
