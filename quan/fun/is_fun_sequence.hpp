3d98707479a0deb29a0befbe286392c7e74fa4b2
skyscraper <kwikius@yahoo.com>
2013-11-17 15:18:58 +0000

    make std::tuple a model of fun_sequence

--- quan/fun/is_fun_sequence.hpp
+++ quan/fun/is_fun_sequence.hpp
@@ -2,13 +2,19 @@
 #define QUAN_FUN_IS_FUN_SEQUENCE_HPP_INCLUDED
 
 #include <type_traits>
-#include <type_traits>
-#include <type_traits>
+ #include <tuple>
+/*
+ to make a model of fun::Sequence
 
+ // sequence requirements are
+   num_elements and at?
+ 
+*/
 namespace quan{ namespace fun{
 
    template <typename T>
    struct is_fun_sequence_impl : std::false_type{};
+
    template <typename T>
    struct is_fun_sequence : is_fun_sequence_impl<
       typename std::remove_const<
@@ -16,6 +22,9 @@ namespace quan{ namespace fun{
       >::type
    >{};
 
+   
+   template<typename... Elements> 
+   struct is_fun_sequence_impl<std::tuple<Elements...> > : std::true_type{};
 }}//quan::fun
 
 #endif
