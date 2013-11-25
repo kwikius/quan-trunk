d0b58d09c6ebdea62fb8d1d507a6169716d3c2a4
skyscraper <kwikius@yahoo.com>
2013-11-25 14:08:00 +0000

    formatting only

--- quan/stm32/usart/irq_handler.hpp
+++ quan/stm32/usart/irq_handler.hpp
@@ -9,7 +9,7 @@ namespace quan{ namespace stm32{ namespace usart{
    inline void irq_handler()
    {
       typedef typename SerialPort::usart_type usart_type;
-    //  if (usart_type::get()->cr1. template bb_getbit<5>() ) { 
+   
       if(SerialPort::ll_rxneie_is_enabled()){
             // received data?
          if(SerialPort::ll_rx_reg_full()){
