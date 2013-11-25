10a191196a900c0bf55b27293207864c6f93160b
skyscraper <kwikius@yahoo.com>
2013-11-25 14:07:18 +0000

    now brr value is calced direct by detil::get_baud_rate_values

--- quan/stm32/usart/detail/setting_to_actions/baud_rate.hpp
+++ quan/stm32/usart/detail/setting_to_actions/baud_rate.hpp
@@ -11,7 +11,10 @@ stm32f05 ref_man
 25.7.4 Baud rate register (USART_BRR)
 
 n.bF0 Baudrate can only be set when Usart is disabled
+
+
 */
+
 namespace quan{ namespace stm32{ namespace usart{namespace detail{
 
    template <uint32_t BaudRate, bool Over8>
@@ -22,13 +25,19 @@ namespace quan{ namespace stm32{ namespace usart{namespace detail{
          typedef typename quan::stm32::detail::make_boolean_action<
             Over8, typename U::cr1_type,15
          >::type over8_type;
-
+#if 0
          typedef quan::stm32::usart::detail::get_baud_rate_values<U,BaudRate,Over8> reg_values;
          typedef typename U::value_type value_type;
 
+         static constexpr uint64_t test_shifted = reg_values::mantissa << 4;
+         static_assert( (test_shifted == static_cast<value_type>(test_shifted)) , "lopped values off");
+         static_assert( ((reg_values::mantissa << 4) & reg_values::fraction) ==0, "lopped off 1");
          static constexpr value_type mask = (reg_values::mantissa << 4) | reg_values::fraction ;
+#endif
          typedef typename quan::stm32::detail::periph_reg_action<
-            typename U::brr_type,quan::meta::equals,mask
+            typename U::brr_type,
+            quan::meta::equals,
+            quan::stm32::usart::detail::get_baud_rate_values<U,BaudRate,Over8>::value
          > action_type;
 
          typedef typename quan::meta::push_back<ListOfActions,over8_type>::type list1_type;
