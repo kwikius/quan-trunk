406e68d483a741333558d0a550af2dcb0a0d6613
skyscraper <kwikius@yahoo.com>
2014-02-19 09:58:06 +0000

    add gauss unit

--- quan/out/magnetic_flux_density.hpp
+++ quan/out/magnetic_flux_density.hpp
@@ -16,5 +16,18 @@
 #include <quan/fixed_quantity/io/output.hpp>
 
 namespace quan{ namespace meta{
+
+   namespace quan{ namespace meta{
+    inline
+    std::ostream&
+    operator <<(
+        std::ostream & os,
+        quan::meta::components::of_magnetic_flux_desnity::non_si_unit::gauss
+    )
+    {
+        os << "gauss";
+        return os;
+    }
+
 }}//quan::meta
 #endif
