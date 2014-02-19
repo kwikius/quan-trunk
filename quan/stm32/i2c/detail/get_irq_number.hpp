8415b6edea12fc3ad3217733fc195cdbdd50085a
skyscraper <kwikius@yahoo.com>
2014-02-19 10:20:41 +0000

    additions to stm32 lib

---
+++ quan/stm32/i2c/detail/get_irq_number.hpp
@@ -0,0 +1,39 @@
+#ifndef QUAN_STM32_I2C_DETAIL_GET_IRQ_NUMBER_HPP_INCLUDED
+#define QUAN_STM32_I2C_DETAIL_GET_IRQ_NUMBER_HPP_INCLUDED
+
+#include <quan/stm32/config.hpp>
+
+#if defined QUAN_STM32F4
+#include <stm32f4xx.h>
+#elif defined QUAN_STM32F3
+#include <stm32f3xx.h>
+#elif defined QUAN_STM32F0
+#include <stm32f0xx.h>
+#else
+#error need to define stm32 processor
+#endif
+#include <type_traits>
+#include <quan/stm32/i2c/typedefs.hpp>
+
+namespace quan{ namespace stm32{ namespace i2c{ namespace detail{
+
+   template < typename I2C>
+   struct get_event_irq_number;
+   template < typename I2C>
+   struct get_error_irq_number;
+#if defined QUAN_STM32_HAS_I2C1
+   template <> struct get_event_irq_number<quan::stm32::i2c1> : std::integral_constant<IRQn_Type,I2C1_EV_IRQn>{};
+   template <> struct get_error_irq_number<quan::stm32::i2c1> : std::integral_constant<IRQn_Type,I2C1_ER_IRQn>{};
+#endif
+#if defined QUAN_STM32_HAS_I2C2
+   template <> struct get_event_irq_number<quan::stm32::i2c2> : std::integral_constant<IRQn_Type,I2C2_EV_IRQn>{};
+   template <> struct get_error_irq_number<quan::stm32::i2c2> : std::integral_constant<IRQn_Type,I2C2_ER_IRQn>{};
+#endif
+#if defined QUAN_STM32_HAS_I2C3
+   template <> struct get_event_irq_number<quan::stm32::i2c3> : std::integral_constant<IRQn_Type,I2C3_EV_IRQn>{};
+   template <> struct get_error_irq_number<quan::stm32::i2c3> : std::integral_constant<IRQn_Type,I2C3_ER_IRQn>{};
+#endif
+
+}}}} // quan::stm32::i2c::detail
+
+#endif // QUAN_STM32_I2C_DETAIL_GET_IRQ_NUMBER_HPP_INCLUDED
---
+++ quan/stm32/systick.hpp
@@ -0,0 +1,46 @@
+#ifndef QUAN_STM32_SYSTICK_HPP_INCLUDED
+#define QUAN_STM32_SYSTICK_HPP_INCLUDED
+
+#include <cstdint>
+#include <quan/time.hpp>
+
+#if defined QUAN_STM32F4
+#include <stm32f4xx.h>
+#elif defined QUAN_STM32F3
+#include <stm32f3xx.h>
+#elif defined QUAN_STM32F0
+#include <stm32f0xx.h>
+#else
+#error need to define stm32 processor
+#endif
+
+namespace quan{namespace stm32{ namespace detail{
+
+   inline void disable_systick_irq()
+   {
+      SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;
+   }
+   inline void enable_systick_irq()
+   {
+      SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
+   }
+   struct systick_tick{
+      static volatile int64_t current;
+   };
+   
+}}}
+
+namespace quan{ namespace stm32{
+
+   inline quan::time_<int64_t>::ms millis()
+   {
+      int64_t result;
+      quan::stm32::detail::disable_systick_irq();
+      result = stm32::detail::systick_tick::current;
+      quan::stm32::detail::enable_systick_irq();
+      return quan::time_<int64_t>::ms{result};
+   }
+
+}}
+
+#endif // QUAN_STM32_SYSTICK_HPP_INCLUDED
