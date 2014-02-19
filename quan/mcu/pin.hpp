4488ec3bd64b19daff39bad87fe45dd26da1f696
skyscraper <kwikius@yahoo.com>
2014-02-19 10:03:00 +0000

    adjust value_type for comparsion

--- quan/mcu/pin.hpp
+++ quan/mcu/pin.hpp
@@ -28,7 +28,7 @@ namespace quan{  namespace mcu{
   template <typename Port,typename Port::value_type Pos> struct pin{
       typedef Port port_type;
       typedef typename port_type::value_type value_type;
-      static_assert( (Pos < quan::meta::numbits<value_type>::value) ,
+      static_assert( Pos < static_cast<typename Port::value_type>(quan::meta::numbits<value_type>::value) ,
          "bit pos out of range in quan::mcu::pin" );
       static const typename Port::value_type pin_value = Pos;
   };
