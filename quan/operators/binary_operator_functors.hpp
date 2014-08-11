c010184084003493be99a7c7b9211f4324e522df
skyscraper <kwikius@yahoo.com>
2014-08-11 14:04:30 +0100

    more copyrighting stuff

--- quan/operators/binary_operator_functors.hpp
+++ quan/operators/binary_operator_functors.hpp
@@ -3,7 +3,22 @@
 #endif
 #ifndef QUAN_BINARY_OPERATOR_FUNCTORS_HPP_INCLUDED
 #define QUAN_BINARY_OPERATOR_FUNCTORS_HPP_INCLUDED
-//  Copyright (C) Andy Little, White Light Device 2003.
+/*
+ Copyright (c) 2003-2014 Andy Little.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see http://www.gnu.org/licenses./
+ */
 //  Permission to copy, use, modify, sell and distribute this software
 //  is granted provided this copyright notice appears in all copies.
 //  This software is provided "as is" without express or implied warranty,
