bc008c2c7b4247d4642d046c2d3b40db613b8c28
skyscraper <kwikius@yahoo.com>
2014-08-11 13:43:53 +0100

    added copyright info to hpps

--- quan/stm32/usart/detail/setting_to_actions/baud_rate.hpp
+++ quan/stm32/usart/detail/setting_to_actions/baud_rate.hpp
@@ -1,5 +1,21 @@
 #ifndef QUAN_STM32_USART_DETAIL_SETTING_TO_ACTIONS_BAUD_RATE_HPP_INCLUDED
 #define QUAN_STM32_USART_DETAIL_SETTING_TO_ACTIONS_BAUD_RATE_HPP_INCLUDED
+/*
+ Copyright (c) 2003-2014 Andy Little.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see http://www.gnu.org/licenses./
+ */
 
 #include <quan/stm32/usart/detail/setting_to_actions_fwd.hpp>
 #include <quan/stm32/detail/make_boolean_action.hpp>
--- quan_matters/test/bigint/ll_bigint_test.hpp
+++ quan_matters/test/bigint/ll_bigint_test.hpp
@@ -1,5 +1,23 @@
-//#define QUAN_LL_BIGINT_USE_SMALLINT_IMPL
- 
+
+#ifndef QUAN_MATTERS_TEST_LL_BIGINT_TEST1_HPP_INCLUDED
+#define QUAN_MATTERS_TEST_LL_BIGINT_TEST1_HPP_INCLUDED
+/*
+ Copyright (c) 2003-2014 Andy Little.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see http://www.gnu.org/licenses./
+ */
+
 #include <quan_matters/test/simple_test.hpp>
 
 #include <quan/detail/ll_bigint.hpp>
@@ -8,3 +26,5 @@ namespace quan{ namespace test{
 
       void ll_bigint_test();
 }}
+#endif
+
