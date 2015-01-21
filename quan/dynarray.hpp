315b78cf00ff60dde8b5ef74ed6b8bbda36796f8
skyscraper <kwikius@yahoo.com>
2015-01-21 09:21:04 +0000

    modify dynarray for FreeRTOS

--- quan/dynarray.hpp
+++ quan/dynarray.hpp
@@ -1,5 +1,5 @@
-#ifndef QUAN_STACK_ARRAY_HPP_INCLUDED
-#define QUAN_STACK_ARRAY_HPP_INCLUDED
+#ifndef QUAN_DYNARRAY_HPP_INCLUDED
+#define QUAN_DYNARRAY_HPP_INCLUDED
 /*
  Copyright (c) 2003-2014 Andy Little.
 
@@ -19,20 +19,31 @@
 //size_t
 #include <cstddef>
 #include <cstdlib>
+
+#ifdef QUAN_FREERTOS
+#include "FreeRTOS.h"
+extern "C" void vPortFree( void *pv );
+extern "C"  void * pvPortMalloc(size_t n);
+#endif
  
 namespace  quan {
 /*
- simple class for allocating on the stack and cleaning itseld up
+ simple class for allocating on the stack and cleaning itself up
 when out of scope.
 requires a ctor function to call on failure to alloc
 / todo optimise so only size of pointer when empty ( store size as 1st elem maybe)
+ For RTOS will need to use thread safe alloc and free funs rather than malloc and free
 */
 template <typename T>
 struct dynarray {
    dynarray (size_t N, void (*pferror) ())
       : m_num_elements {N}
    {
-      m_ptr = (T*) malloc (N * sizeof (T));
+#ifdef QUAN_FREERTOS
+     m_ptr = (T*) pvPortMalloc (N * sizeof (T));
+#else
+     m_ptr = (T*) ::malloc (N * sizeof (T));
+#endif
       if (m_ptr == nullptr) {
          pferror();
          m_num_elements = 0;
@@ -40,25 +51,34 @@ struct dynarray {
    }
    ~dynarray()
    {
-      ::free (m_ptr);
+#ifdef QUAN_FREERTOS
+    ::vPortFree(m_ptr);
+#else
+    ::free (m_ptr);
+#endif
    }
    
    size_t get_num_elements() const
    {
       return m_num_elements;
    }
+
    void free()
    {
+#ifdef QUAN_FREERTOS
+    ::vPortFree(m_ptr);
+#else
       ::free (m_ptr);
+#endif
       m_ptr = nullptr;
       m_num_elements = 0;
    }
-   
-   
+  
    T* get() const
    {
       return m_ptr;
    }
+
    bool realloc (size_t N)
    {
       // check for 0 ?
@@ -68,24 +88,25 @@ struct dynarray {
       if (N == m_num_elements) {
          return true;
       } else {
-         T* new_rep = (T*) ::realloc (m_ptr,N * sizeof (T));
-         if (new_rep == nullptr) {
-            // memory stays the same
-            return false;
-         } else {
-            m_ptr = new_rep;
-            m_num_elements = N;
-            /*
-                      for ( uint32_t i = 0; i < m_num_elements; ++i){
-                m_ptr[i] = T{0};
-               }
-            */
-            return true;
-         }
+#ifdef QUAN_FREERTOS
+       T* new_rep = (T*) ::pvPortMalloc (N * sizeof (T));
+#else
+       T* new_rep = (T*) ::realloc (m_ptr,N * sizeof (T));
+#endif
+        if ( new_rep != nullptr ){
+#ifdef QUAN_FREERTOS
+         ::vPortFree(m_ptr);
+#endif
+         m_ptr = new_rep;
+         m_num_elements = N;
+         return true;
+        }else{
+             // memory stays the same
+          return false;
+        }
       }
    }
-   
-   
+  
    bool realloc (size_t N, void (*pferror) ())
    {
       if (this->realloc (N)) {
@@ -95,7 +116,6 @@ struct dynarray {
          return false;
       }
    }
-   
    
    bool good() const {
       return m_ptr != nullptr;
@@ -108,6 +128,6 @@ private:
 };
 } //quan
  
-#endif // QUAN_STACK_ARRAY_HPP_INCLUDED
+#endif // QUAN_DYNARRAY_HPP_INCLUDED
  
  
