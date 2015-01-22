aace4322ad071d11d603c2448dbae4b466f98f32
skyscraper <kwikius@yahoo.com>
2015-01-22 12:01:00 +0000

    millis() for freertos without using systick

---
+++ quan/stm32/freertos/millis.hpp
@@ -0,0 +1,17 @@
+#ifndef QUAN_STM32_FREERTOS_MILLIS_HPP_INCLUDED
+#define QUAN_STM32_FREERTOS_MILLIS_HPP_INCLUDED
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <quan/time.hpp>
+
+namespace quan{ namespace stm32{
+   
+   inline quan::time_<int64_t>::ms millis()
+   {
+      return quan::time_<int64_t>::ms{(static_cast<int64_t>(xTaskGetTickCount()) * 1000)/configTICK_RATE_HZ};
+   }
+
+}}
+
+#endif //QUAN_STM32_FREERTOS_MILLIS_HPP_INCLUDED
