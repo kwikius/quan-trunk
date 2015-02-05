925192bf8bb80c2e1f1cbf690ee99851e5d0f354
skyscraper <kwikius@yahoo.com>
2015-02-05 10:50:41 +0000

    fix quan/uav/get_bearing to template

--- quan/uav/get_bearing.hpp
+++ quan/uav/get_bearing.hpp
@@ -20,26 +20,26 @@
 #include <quan/uav/position.hpp>
 #include <quan/atan2.hpp>
 
-
 namespace quan{ namespace uav{
 
  // gives bearing in degrees relative to grid north
  // positive is clockwise
 
+   template <typename PositionType>
 	inline 
-	quan::angle::deg 
-	get_bearing(quan::uav::position const & gps1, quan::uav::position const & gps2)
+	typename PositionType::angle_type 
+	get_bearing(PositionType const & gps1, PositionType const & gps2)
 	{
         // check for too small to use...
         // if dist is less than N meters == N meters = dlon * R
-        quan::angle::rad const dLon = gps2.lon - gps1.lon;
-
+        typedef typename PositionType::angle_type angle_type;
+        angle_type const dLon = gps2.lon - gps1.lon;
         
         auto const cos_gps2_lat =  cos(gps2.lat);
         auto const y = sin(dLon) * cos_gps2_lat;
         auto const x = cos(gps1.lat) * sin(gps2.lat)
                               - sin(gps1.lat) * cos_gps2_lat * cos(dLon);
-        quan::angle::deg result = quan::atan2(y,x);
+        angle_type result  = quan::atan2(y,x);
         while (result < quan::angle::deg(0)){
            result += quan::angle::deg(360);
         }
