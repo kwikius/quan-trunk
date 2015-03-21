c45a6cade6c0011cf7703bfb67bf8764f69c0eb4
skyscraper <kwikius@yahoo.com>
2015-03-21 08:01:06 +0000

    stm32 lightweight serial port and separteion of user io eros from internal errors

---
+++ quan/user.hpp
@@ -0,0 +1,16 @@
+#ifndef QUAN_USER_HPP_INCLUDED
+#define QUAN_USER_HPP_INCLUDED
+
+#include <cstdint>
+
+namespace quan{ 
+// for the app to define
+   void user_message (const char* str);
+   void user_error(const char* str);
+   void user_flush_sptx();
+   uint32_t user_in_avail();
+   char user_get();
+   void report_errors();
+}
+
+#endif // QUAN_USER_HPP_INCLUDED
