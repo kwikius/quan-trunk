a18a1b5451d71ca33aa1955723ccb3a811458514
skyscraper <kwikius@yahoo.com>
2015-03-29 11:24:38 +0100

    add clean up buff on throw

--- quan/fs/get_abs_file_path.hpp
+++ quan/fs/get_abs_file_path.hpp
@@ -36,16 +36,19 @@ namespace quan{ namespace fs{
 
   inline std::string get_abs_file_path(std::string const & str)
   {
+    
 #if defined (linux) || defined (__linux)
      char* buf = new char[PATH_MAX];
      errno = 0;
-     if( realpath(str.c_str(),buf) !=0){
+     if( ::realpath(str.c_str(),buf) !=0){
        std::string ret = buf;
        delete [] buf;
        return ret;
      }
      else{
-       throw std::runtime_error(std::strerror(errno));
+      delete [] buf;
+      //return "";
+      throw std::runtime_error(std::strerror(errno));
      }
 #else
  #error "need to define get_abs_file_path for this platform"
