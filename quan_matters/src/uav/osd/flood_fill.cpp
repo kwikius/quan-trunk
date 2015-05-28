ea75a25c2d0bb672a68ff70d1dffa51be6b8806f
skyscraper <kwikius@yahoo.com>
2015-05-28 11:03:50 +0100

    floodfill not working as yet

--- quan_matters/src/uav/osd/flood_fill.cpp
+++ quan_matters/src/uav/osd/flood_fill.cpp
@@ -1,12 +1,7 @@
 // N.B. need alternative to stack
 // malloc a fixed size stack
-#include <stack>
-
+#include <quan/utility/rt_stack.hpp>
 #include <quan/uav/osd/api.hpp>
-
-//using namespace quan::uav::osd;
-
-///namespace quan{ namespace uav { namespace osd{
 
 void quan::uav::osd::flood_fill(
    pxp_type const & start_pos, 
@@ -30,7 +25,7 @@ void quan::uav::osd::flood_fill(
       return;
    }
 
-   std::stack<pxp_type> stack;
+   quan::rt_stack<pxp_type> stack(max_count);
    stack.push(raw_pxp);
    int count = 0;
    while (! stack.empty()){
@@ -38,8 +33,8 @@ void quan::uav::osd::flood_fill(
       if (++ count == max_count){
          return;
       }
-      auto n = stack.top();
-      stack.pop();
+      pxp_type n;
+      stack.pop(n);
       set_pixel_raw(n,new_colour);
       if ( n.x > 0){
          auto west = pxp_type {n.x-1,n.y};
@@ -47,7 +42,7 @@ void quan::uav::osd::flood_fill(
             stack.push(west);
          }
       } 
-      if ( n.x < static_cast<int>((display_size.x -1))){
+      if ( n.x < static_cast<int>(display_size.x -1)){
          auto east = pxp_type {n.x+1,n.y};
          if ( get_pixel_raw(east) != new_colour){
             stack.push(east);
@@ -59,7 +54,7 @@ void quan::uav::osd::flood_fill(
             stack.push(north);
          }
       }
-      if( n.y < static_cast<int>((display_size.y -1))){
+      if( n.y < static_cast<int>(display_size.y -1)){
        auto south = pxp_type {n.x,n.y+1};
        if ( get_pixel_raw(south) != new_colour){
             stack.push(south);
@@ -67,5 +62,3 @@ void quan::uav::osd::flood_fill(
      }
    }
 }
-
-//}}} //quan::uav::osd
