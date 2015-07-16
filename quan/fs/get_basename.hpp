5f8f2861e60a96bf6a959b512942c3aaacaf1f54
skyscraper <kwikius@yahoo.com>
2015-07-16 13:53:14 +0100

    get fs::get_basename working in mingw and avoid strdup

--- quan/fs/get_basename.hpp
+++ quan/fs/get_basename.hpp
@@ -1,11 +1,16 @@
 #ifndef QUAN_FS_GET_BASENAME_HPP_INCLUDED
 #define QUAN_FS_GET_BASENAME_HPP_INCLUDED
 
-#if defined (linux) || defined (__linux)
+#include <quan/config.hpp>
+
+#if ((defined QUAN_OS_LINUX)  || (defined(__MINGW32__)))
 #include <libgen.h>
+#include <malloc.h>
 #else
+// hint for windows use splitpath?
 #error "need to define get_basename for this platform"
 #endif
+
 #include <string>
 #include <cstring>
 
@@ -13,24 +18,31 @@ namespace quan{ namespace fs{
 
    inline std::string get_basename(std::string const & path_in)
    {
+#if (defined(QUAN_OS_LINUX)  || defined(__MINGW32__))
      // assert ( (path_in != "") && __LINE__);
-      if (path_in == ""){
+     if (path_in == ""){
             return "";
-      }
-      char* path = strdup(path_in.c_str());
-       if (!path){
-          return "";
-      }
+     }
+     char * path = (char*) ::malloc(path_in.length()+1);
+     if (path == nullptr){
+         return "";
+     }else{
+         strcpy(path,path_in.c_str());
+     }
+
      // assert(path && __LINE__);
-#if defined (linux) || defined (__linux)
+
       char* basename = ::basename(path);
-#else
-#error "need to define get_basename for this platform"
-#endif
+
       std::string result = basename;
       free(path);
       return result;
-   }
-}}
+
+#else
+#error "need to define get_basename for this platform"
+#endif
+       } //fun
+
+}}  // quan::fs
 
 #endif // QUAN_FS_GET_BASENAME_HPP_INCLUDED
