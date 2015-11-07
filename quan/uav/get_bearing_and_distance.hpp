1c0350a8de69fd10a47567122e21a6641b8258ec
skyscraper <kwikius@yahoo.com>
2015-11-07 12:33:33 +0000

    bearing and distance now reintroduced

--- quan/uav/get_bearing_and_distance.hpp
+++ quan/uav/get_bearing_and_distance.hpp
@@ -4,6 +4,7 @@
 #include <quan/length.hpp>
 #include <quan/angle.hpp>
 #include <quan/uav/position.hpp>
+#include <quan/atan2.hpp>
 
 namespace quan{ namespace uav{
 
@@ -12,13 +13,39 @@ namespace quan{ namespace uav{
    to avoid div 0, returns treu if put bearing else false
 */
 
-   template <typename AngleType, typename LengthType>
+   template <typename AngleType, typename LengthType, typename AngleType1, typename LengthType1>
    inline
    bool get_bearing_and_distance(
       quan::uav::position<AngleType, LengthType> const & from, 
       quan::uav::position<AngleType, LengthType> const & to, 
-      AngleType & bearing_out, LengthType & distance_out
+      AngleType1 & bearing_out, LengthType1 & distance_out)
    {
+
+#if 1
+
+//     template <typename PositionType>
+//	inline 
+//	typename PositionType::angle_type 
+//	get_bearing(PositionType const & gps1, PositionType const & gps2)
+//	{
+      quan::angle_<float>::deg  dlat = from.lat - to.lat; // sort diffence for angle
+      quan::angle_<float>::deg  dlon = from.lon - to.lon;
+      constexpr typename quan::length_<float>::m R{6371000.f};
+      quan::length_<float>::m  rlat = cos(quan::angle_<float>::rad{abs(to.lat)}) * R;
+      quan::length_<float>::m  dx = (dlon * rlat) / quan::angle_<float>::rad{1};
+      quan::length_<float>::m  dy =  (dlat * R ) / quan::angle_<float>::rad{1.f};
+      quan::length_<float>::m  distance = quan::sqrt(dx * dx + dy * dy);
+      distance_out = distance;
+      if ( abs(distance) > quan::length_<float>::m{2.f}){
+        bearing_out = quan::atan2(-dy, dx) + quan::angle::pi/2;
+        return true;
+      }else{
+        bearing_out = AngleType1{0};
+        return false;
+      }
+
+#else
+// note that to and from are reversed from above impl
        typedef AngleType angle_type;
        typedef LengthType length_type;
 
@@ -37,6 +64,8 @@ namespace quan{ namespace uav{
        }else{
          return false;
        }
+
+#endif
    }
 
 }}
