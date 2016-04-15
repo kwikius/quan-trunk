2ed6f54fb311c4ffc80769fabf7b918c1020beae
skyscraper <kwikius@yahoo.com>
2016-04-15 19:59:11 +0100

    tentatively add stm32f0 version

--- quan/stm32/get_raw_timer_frequency.hpp
+++ quan/stm32/get_raw_timer_frequency.hpp
@@ -9,9 +9,10 @@
 // however not to sure re other processors eg f3
 // Anyway as it stands raw timer frequency on f4
 // is module_bus_freq * 2
-#if defined QUAN_STM32F4
+
 namespace quan{ namespace stm32{
 
+#if defined QUAN_STM32F4
    //return frequency in Hz as integer
    template <typename TimerModule>
    inline constexpr uint32_t get_raw_timer_frequency()
@@ -19,9 +20,19 @@ namespace quan{ namespace stm32{
       static_assert(quan::is_model_of<quan::stm32::Tim,TimerModule>::value ==true,"only useful for timers");
       return quan::stm32::get_module_bus_frequency<TimerModule>() * 2U;
    }
-}}
+
+#elif defined QUAN_STM32F0
+   // think freq is same
+   template <typename TimerModule>
+   inline constexpr uint32_t get_raw_timer_frequency()
+   {
+      static_assert(quan::is_model_of<quan::stm32::Tim,TimerModule>::value ==true,"only useful for timers");
+      return quan::stm32::get_module_bus_frequency<TimerModule>();
+   }
 #else
 #error need to check calcs for other processors
 #endif
+
+}}
 
 #endif // QUAN_STM32_GET_RAW_TIMER_FREQUENCY_HPP_INCLUDED
