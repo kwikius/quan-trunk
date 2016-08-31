49d69a1bca804b526407936f9f6ba80aef4e1751
Andy Little <kwikius@yahoo.com>
2016-08-31 22:17:31 +0100

    add key_pressed function for linux

---
+++ quan/key_was_pressed.hpp
@@ -0,0 +1,10 @@
+#ifndef QUAN_KEY_WAS_PRESSED_HPP_INCLUDED
+#define QUAN_KEY_WAS_PRESSED_HPP_INCLUDED
+
+namespace quan{
+
+    bool key_was_pressed();
+
+}
+
+#endif // QUAN_KEY_WAS_PRESSED_HPP_INCLUDED
---
+++ quan_matters/src/linux/key_was_pressed.cpp
@@ -0,0 +1,36 @@
+// Linux impl
+#include <termios.h>
+#include <cstdio>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <quan/key_was_pressed.hpp>
+// from
+// http://stackoverflow.com/questions/22166074/is-there-a-way-to-detect-if-a-key-has-been-pressed
+namespace quan{
+
+   bool key_was_pressed()
+   {
+     termios oldt;
+     tcgetattr(STDIN_FILENO, &oldt);
+
+     termios newt = oldt;
+     newt.c_lflag &= ~(ICANON | ECHO);
+     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+     int oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+
+     fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+     int ch = getchar();
+     bool input = ch != EOF;
+
+     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+     fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+     if(input){
+        ungetc(ch, stdin);
+     }
+     return input;
+   }
+}
