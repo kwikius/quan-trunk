5ba60572a6d3b287f8e2f9282b95f8b32d6d7b7c
Andy Little <kwikius@yahoo.com>
2016-11-03 21:46:14 +0000

    Make spi a model of Spi and Module

--- quan/stm32/spi/module.hpp
+++ quan/stm32/spi/module.hpp
@@ -1,6 +1,9 @@
 #ifndef QUAN_STM32_SPI_MODULE_HPP_INCLUDED
 #define QUAN_STM32_SPI_MODULE_HPP_INCLUDED
 
+#include <quan/stm32/config.hpp>
+#include <quan/stm32/module.hpp>
+
 #if defined QUAN_STM32F4
 #include <quan/stm32/f4/spi/module.hpp>
 #elif defined QUAN_STM32F3
@@ -10,4 +13,23 @@
 #else
 #error 
 #endif
+
+namespace quan{ namespace impl{
+
+   // make port a model of Spi
+   template <uint32_t Address>
+   struct is_model_of_impl<
+			quan::stm32::Spi,
+			quan::stm32::spi::module<Address> 
+	> : quan::meta::true_{};
+
+   // make a model of Module
+   template <uint32_t Address>
+   struct is_model_of_impl<
+			quan::stm32::Module,
+			quan::stm32::spi::module<Address> 
+	> : quan::meta::true_{};
+
+}}
+
 #endif // QUAN_STM32_MODULE_HPP_INCLUDED
