5ced664d4d939ac56292df5406fdfe4372683c10
Andy Little <kwikius@yahoo.com>
2017-02-04 09:17:17 +0000

    remove extraneous output from test

--- quan_matters/test/fixed_quantity/semantics/dimless_multiply.cpp
+++ quan_matters/test/fixed_quantity/semantics/dimless_multiply.cpp
@@ -272,7 +272,7 @@ namespace{
 //#########################promoted#########################
     //  QUAN_CHECK((typeid(mul::eval<int,int>()(1,1)) == typeid(long long)));
 //#########################################################################
-        std::cout << typeid(mul::eval<int,int>()(1,1)).name() <<'\n';
+    //    std::cout << typeid(mul::eval<int,int>()(1,1)).name() <<'\n';
        int val1 = mul::eval<int,int>()(1,1);
        int val1_chk = (1 * 1) * ((7 / 1) * (9 / 1));
        QUAN_CHECK_EQUAL(val1,val1_chk);
@@ -622,7 +622,7 @@ namespace{
      //  std::cout << rat::numerator << "/ " << rat::denominator <<'\n';
 //##################modified######################
     //   QUAN_CHECK( (typeid(mul::eval<int,int>()(1,1)) == typeid(long long)));
-        std::cout << typeid(mul::eval<int,int>()(1,1)).name() <<'\n';
+     //   std::cout << typeid(mul::eval<int,int>()(1,1)).name() <<'\n';
 //###########################################
        double in_L = 2.8f;
        double in_R = 7.2f;
