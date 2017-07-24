60848c802aae33bfab84dd5e2b771e59969f23bb
Andy Little <kwikius@yahoo.com>
2017-07-24 11:09:49 +0100

    add std::common_type for AVR

---
+++ quan/std/tr1/common_type.hpp
@@ -0,0 +1,19 @@
+#ifndef QUAN_STD_TR1_COMMON_TYPE_HPP_INCLUDED
+#define QUAN_STD_TR1_COMMON_TYPE_HPP_INCLUDED
+
+#include <quan/config.hpp>
+#ifdef QUAN_USE_QUAN_STD_TR1
+
+#include <quan/meta/binary_op.hpp>
+
+namespace std{
+
+   template <typename TL, typename TR>
+   struct common_type : quan::meta::binary_op<TL,quan::meta::minus,TR>{};
+}
+
+#else
+#include <type_traits>
+#endif
+
+#endif // QUAN_STD_TR1_COMMON_TYPE_HPP_INCLUDED
