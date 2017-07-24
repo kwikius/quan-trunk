0b2a5af71f027526101e16335681c481abdb5ba6
Andy Little <kwikius@yahoo.com>
2017-07-24 11:10:35 +0100

    add pll constants to test

---
+++ quan_matters/test/stm32/more_pll_stuff.cpp
@@ -0,0 +1,73 @@
+
+#define QUAN_STM32F4
+//#define QUAN_STM32_SYSCLK_Hz 168000000
+#define QUAN_STM32_SYSCLK_Hz 72000000
+
+#include <quan/stm32/f4/detail/system_pll_constants.hpp>
+#include <quan/stm32/sys_freq.hpp>
+#include <quan/stm32/get_module_bus_frequency.hpp>
+#include <quan/time.hpp>
+#include <quan/frequency.hpp>
+#include <quan_matters/test/test.hpp>
+
+typedef quan::stm32::detail::system_pll_constants pll_constants;
+
+constexpr uint32_t get_pllm(){ return pll_constants::pllm;}
+constexpr uint32_t get_plln(){ return pll_constants::plln;}
+constexpr uint32_t get_pllp(){ return pll_constants::pllp;}
+constexpr uint32_t get_pllq(){ return pll_constants::pllq;}
+
+constexpr uint32_t get_ahb1_divider() { return QUAN_STM32_AHB1_DIVISOR;}
+constexpr uint32_t get_ahb2_divider() { return QUAN_STM32_AHB2_DIVISOR;}
+
+constexpr uint32_t get_ahb3_divider() { return QUAN_STM32_AHB3_DIVISOR;}
+
+constexpr uint32_t get_apb1_divider() { return QUAN_STM32_APB1_DIVISOR;}
+constexpr uint32_t get_apb2_divider() { return QUAN_STM32_APB2_DIVISOR;}
+
+
+int errors = 0;
+
+/*
+  testing reduced frequency 
+*/
+
+int main()
+{
+ # if 0
+   std::cout << "pllm = " << get_pllm() <<'\n';
+   std::cout << "plln = " << get_plln() <<'\n';
+   std::cout << "pllp = " << get_pllp() <<'\n';
+   std::cout << "pllq = " << get_pllq() <<'\n';
+#endif 
+
+   uint32_t constexpr hse_value = QUAN_STM32_SOURCE_CLOCK_Hz;
+   //std::cout << "HSE = " << hse_value <<'\n';
+   uint32_t constexpr target_sysclk = QUAN_STM32_SYSCLK_Hz;
+
+   uint32_t constexpr pll_vco = (hse_value / get_pllm() ) * get_plln();
+   uint32_t constexpr sys_clk = pll_vco / get_pllp();
+   uint32_t constexpr usb_clock = pll_vco / get_pllq();
+
+   QUAN_CHECK( sys_clk == target_sysclk)
+
+   QUAN_CHECK( usb_clock == 48000000)
+
+   uint32_t const expected_ahb1_freq = target_sysclk;
+   QUAN_CHECK(quan::stm32::get_bus_frequency<quan::stm32::detail::ahb1>() == expected_ahb1_freq);
+
+   uint32_t const expected_ahb2_freq = target_sysclk;
+   QUAN_CHECK(quan::stm32::get_bus_frequency<quan::stm32::detail::ahb2>() == expected_ahb2_freq);
+
+   uint32_t const expected_ahb3_freq = target_sysclk;
+   QUAN_CHECK(quan::stm32::get_bus_frequency<quan::stm32::detail::ahb3>() == expected_ahb3_freq);
+
+   uint32_t const expected_apb1_freq = quan::stm32::get_bus_frequency<quan::stm32::detail::apb1>();
+   QUAN_CHECK(expected_apb1_freq == sys_clk / 4); 
+   
+   uint32_t expected_apb2_freq = quan::stm32::get_bus_frequency<quan::stm32::detail::apb2>();
+   QUAN_CHECK(expected_apb2_freq == sys_clk / 2);
+
+   
+   EPILOGUE
+}
---
+++ quan_matters/test/stm32/pll_constants.cpp
@@ -0,0 +1,37 @@
+
+#define QUAN_STM32F4
+//#define QUAN_STM32_SYSCLK_Hz 168000000
+#define QUAN_STM32_SYSCLK_Hz 84000000
+
+#include <quan/stm32/f4/detail/system_pll_constants.hpp>
+
+typedef quan::stm32::detail::system_pll_constants pll_constants;
+
+constexpr uint32_t get_pllm(){ return pll_constants::pllm;}
+constexpr uint32_t get_plln(){ return pll_constants::plln;}
+constexpr uint32_t get_pllp(){ return pll_constants::pllp;}
+constexpr uint32_t get_pllq(){ return pll_constants::pllq;}
+
+constexpr uint32_t get_ahb1_divider() { return QUAN_STM32_AHB1_DIVISOR;}
+constexpr uint32_t get_ahb2_divider() { return QUAN_STM32_AHB2_DIVISOR;}
+
+constexpr uint32_t get_ahb3_divider() { return QUAN_STM32_AHB3_DIVISOR;}
+
+constexpr uint32_t get_apb1_divider() { return QUAN_STM32_APB1_DIVISOR;}
+constexpr uint32_t get_apb2_divider() { return QUAN_STM32_APB2_DIVISOR;}
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "pllm = " << get_pllm() <<'\n';
+  std::cout << "plln = " << get_plln() <<'\n';
+  std::cout << "pllp = " << get_pllp() <<'\n';
+  std::cout << "pllq = " << get_pllq() <<'\n';
+
+  std::cout << "ahb1_divisor = " << get_ahb1_divider() <<'\n';
+  std::cout << "ahb2_divisor = " << get_ahb2_divider() <<'\n';
+  std::cout << "ahb3_divisor = " << get_ahb3_divider() <<'\n';
+  std::cout << "apb1_divisor = " << get_apb1_divider() <<'\n';
+  std::cout << "apb2_divisor = " << get_apb2_divider() <<'\n';
+}
