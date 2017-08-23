06eb26bfe2fd8dfb28f0c3be2c20dc7dc24e831f
Andy Little <kwikius@yahoo.com>
2017-08-23 09:55:31 +0100

    remove dead code

--- quan/dom/alg/get_element.hpp
+++ quan/dom/alg/get_element.hpp
@@ -4,18 +4,18 @@
 #include <quan/dom/leaf.hpp>
 #include <quan/dom/alg/get_leaf_node.hpp>
 
+/*
+ todo rename to get_value
+   get_value<T>(node,path);
+*/
+
 namespace quan{ namespace dom{
 
     template <typename T, typename ID, typename Path>
     inline
     T get_element(node<ID>* p_in,Path const & path)
     { 
-#if 1
        return get_leaf_node<T>(p_in,path)->get_data();
-#else
-       auto d = get_leaf_node<T>(p_in,path);
-       return d->get_data();
-#endif
     }
     
 }}//quan::dom
