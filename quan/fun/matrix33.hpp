ff5f12f12f00d9947a45fa3c6804ec5575777139
Andy Little <kwikius@yahoo.com>
2018-01-25 11:50:37 +0000

    dont use the specific matrix, prefer the general one

--- quan/fun/matrix33.hpp
+++ quan/fun/matrix33.hpp
@@ -16,7 +16,7 @@
  You should have received a copy of the GNU General Public License
  along with this program. If not, see http://www.gnu.org/licenses./
  */
-
+#error dont include
 #include <quan/fun/vector3.hpp>
 #include <quan/fun/vector9.hpp>
 #include <quan/fun/same_size_seq.hpp>
