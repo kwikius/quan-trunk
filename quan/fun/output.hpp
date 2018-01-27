59d0865346489dbd60bbef49b7f60d5f2c550038
Andy Little <kwikius@yahoo.com>
2018-01-27 17:42:36 +0000

    fusion output comments only

--- quan/fun/output.hpp
+++ quan/fun/output.hpp
@@ -19,31 +19,32 @@
 
 #include <iostream>
 #include <string>
-namespace quan{ namespace fun{namespace detail{
+namespace quan{ namespace fun{
+   namespace detail{
 
    template <typename CharType>
-   struct output_{
-      std::basic_ostream<CharType> & os;
-      std::basic_string<CharType> sep;
-      output_(
-         std::basic_ostream<CharType> & os_in,
-         std::basic_string<CharType> const & sep_in
-      ):os(os_in), sep(sep_in){}
+      struct output_{
+         std::basic_ostream<CharType> & os;
+         std::basic_string<CharType> sep;
+         output_(
+            std::basic_ostream<CharType> & os_in,
+            std::basic_string<CharType> const & sep_in
+         ):os(os_in), sep(sep_in){}
 
-      template <typename T>
-      struct result{
-         typedef std::basic_ostream<CharType> & type;
+         template <typename T>
+         struct result{
+            typedef std::basic_ostream<CharType> & type;
+         };
+
+         template <typename T>
+         typename result<T>::type
+         operator()(T const & t)const
+         {
+            return os << t << sep;
+         }
+         private:
+         output_ & operator =( output_& in);
       };
-
-      template <typename T>
-      typename result<T>::type
-      operator()(T const & t)const
-      {
-         return os << t << sep;
-      }
-      private:
-      output_ & operator =( output_& in);
-   };
    }//detail
 
    template <typename CharType>
@@ -67,8 +68,7 @@ namespace quan{ namespace fun{namespace detail{
    detail::output_<CharType>
    output(std::basic_ostream<CharType>& os, CharType  sep)
    {
-      std::basic_string<CharType> ss;
-      ss += sep;
+      std::basic_string<CharType> ss = sep;
       return detail::output_<CharType>(os,ss);
    }
 
