de95836bc1410f2018b84ea01f32b89d37f88904
Andy Little <kwikius@yahoo.com>
2018-01-28 13:06:05 +0000

    fusion.fold : comments only

--- quan/fusion/fold.hpp
+++ quan/fusion/fold.hpp
@@ -5,12 +5,21 @@
 #include <utility>
 
 namespace quan{ namespace fusion {
+
+   /*
+      Seq is a model of quan::fusion Sequence
+      init is the initial value of the fold
+      F is a struct with  has a binary operator() which takes 2 arguments by value or const ref
+      F has a member struct result with a member type which is the type of the result of F's operator ()
+   */
    
    template <typename Seq, typename T , typename F>
    inline constexpr
    typename quan::fun::fold_seq::result<Seq,T,F>::type
    fold(Seq && seq, T && init, F&& f)
    {
+      // For simplicity fold cannot mutate its sequence or its init
+      static_assert( std::is_reference<typename quan::fun::fold_seq::result<Seq,T,F>::type>::value == false,"no ref return");
       return quan::fun::fold_seq{}(std::forward<Seq>(seq),std::forward<T>(init),std::forward<F>(f));
    }
 
