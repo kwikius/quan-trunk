1322595bdc74f6fcd32c46d1d392758931e8b3b6
Andy Little <kwikius@yahoo.com>
2018-01-28 13:08:43 +0000

    fuion.make_vector : remove conste ref from arg types

--- quan/fusion/make_vector.hpp
+++ quan/fusion/make_vector.hpp
@@ -2,15 +2,17 @@
 #define QUAN_FUSION_MAKE_VECTOR_HPP_INCLUDED
 
 #include <quan/fun/vector.hpp>
+#include <type_traits>
+#include <quan/meta/strip_cr.hpp>
 
 namespace quan{ namespace fusion{
 
     template <typename ... Args>
     constexpr
     inline
-    quan::fun::vector<Args...> make_vector( Args &&... args)
+    quan::fun::vector<typename quan::meta::strip_cr<Args>::type...> make_vector( Args &&... args)
     {
-        return quan::fun::vector<Args...>(args...);
+        return quan::fun::vector<typename quan::meta::strip_cr<Args>::type...>(args...);
     }
 
 }}
