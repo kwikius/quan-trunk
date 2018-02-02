7f41dfcd350ec3f01cccedfefce854a080a6c168
Andy Little <kwikius@yahoo.com>
2018-02-02 14:54:28 +0000

    fun: make matrix view const only

--- quan/fun/sub_matrix_view.hpp
+++ quan/fun/sub_matrix_view.hpp
@@ -3,55 +3,46 @@
 #define QUAN_FUN_SUB_MATRIX_VIEW_HPP_INCLUDED
 
 #include <quan/fun/is_fun_matrix.hpp>
+#include <quan/fun/seq_arg_type.hpp>
 
 namespace quan { namespace fun{
 
   template <int MatrixRow, int MatrixCol, typename Matrix>
   struct sub_matrix_view{
-        typedef sub_matrix_view type;
-        typedef typename quan::meta::strip_cr<Matrix>::type parent_matrix_type;
-        static constexpr int rows = parent_matrix_type::rows -1;
-        static constexpr int cols = parent_matrix_type::cols -1;
-        typedef typename parent_matrix_type::access_type access_type;
+     typedef sub_matrix_view type;
+     typedef typename quan::meta::strip_cr<Matrix>::type parent_matrix_type;
+     typedef typename seq_arg_type<Matrix>::type arg_type;
+     
+     static constexpr int rows = matrix_row_size<parent_matrix_type>::value -1;
+     static constexpr int cols = matrix_col_size<parent_matrix_type>::value -1;
+     static_assert( rows > 0 && cols > 0, "zero size sub-matrix");
+     typedef typename parent_matrix_type::access_type access_type;
 
-        sub_matrix_view(parent_matrix_type const & in): m_parent_matrix{in}{}
+     constexpr sub_matrix_view(parent_matrix_type const & in): m_parent_matrix{in}{}
 
-        template <int R, int C, typename F = as_value>
-        struct type_at{
-            static int constexpr Rparent = (R < MatrixRow)? R: R + 1;
-            static int constexpr Cparent = (C < MatrixCol)? C: C + 1;
-            typedef typename parent_matrix_type:: template type_at<Rparent,Cparent,F>::type type;
-        };  
-/*
-        template <int R, int C>
-        constexpr 
-        typename quan::where_c< 
-            ((R < rows) && ( C < cols))
-           ,typename type_at<R,C,as_ref>::type
-        >::type
-        at() &
-        {
-          typedef typename type_at<R,C,as_ref> type_at1;
-          int constexpr r = type_at1::Rparent;
-          int constexpr c =type_at1::Cparent;
-          return m_parent_matrix. template at<r,c>();
-        }
-*/
-        template <int R, int C>
-        constexpr 
-        typename quan::where_c< 
-            ((R < rows) && ( C < cols))
-           ,typename type_at<R,C,as_const_ref>::type
-        >::type
-        at() const &
-        {
-          typedef type_at<R,C,as_const_ref> type_atf;
-          int constexpr r = type_atf::Rparent;
-          int constexpr c = type_atf::Cparent;
-          return m_parent_matrix. template at<r,c>();
-        }
-        private:
-        parent_matrix_type const & m_parent_matrix;
+     template <int R, int C, typename F = as_value>
+     struct type_at{
+         static int constexpr Rparent = (R < MatrixRow)? R: R + 1;
+         static int constexpr Cparent = (C < MatrixCol)? C: C + 1;
+         typedef typename parent_matrix_type:: template type_at<Rparent,Cparent,F>::type type;
+     };  
+
+     template <int R, int C>
+     constexpr 
+     typename quan::where_c< 
+         ((R < rows) && ( C < cols))
+        ,typename type_at<R,C,as_value>::type
+     >::type
+     at() const  
+     {
+       typedef type_at<R,C,as_value> type_atf;
+       int constexpr r = type_atf::Rparent;
+       int constexpr c = type_atf::Cparent;
+       return m_parent_matrix. template at<r,c>();
+     }
+     private:
+     // n.b that ref arg_ty[e inhibits constexpr for lvalue
+     parent_matrix_type const & m_parent_matrix;
   };
 
     namespace impl {
