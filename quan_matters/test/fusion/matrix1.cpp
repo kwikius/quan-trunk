c5f741e6545bbcda5611811f705bd87e451514d1
Andy Little <kwikius@yahoo.com>
2018-02-03 11:06:46 +0000

    fusion : more vtests of cofactor

--- quan_matters/test/fusion/matrix1.cpp
+++ quan_matters/test/fusion/matrix1.cpp
@@ -15,7 +15,6 @@
 #include <quan/fixed_quantity/literal.hpp>
 #include <quan/fusion/static_value/out/static_value.hpp>
 
-
 namespace {
 
    template <int64_t N, int64_t D = 1>
@@ -92,95 +91,86 @@ namespace {
       QUAN_CHECK(is_static_value<decltype(v)>)
       QUAN_CHECK(quan::fusion::to_runtime{}(v) == -1054)
 
-    //  std::cout << "v = " << v << '\n';
-/*
+// s00
+      auto const s00 = quan::fusion::make_sub_matrix_view<0,0>(m);
+      QUAN_CHECK((s00.at<0,0>() == 4))
+      QUAN_CHECK((s00.at<0,1>() == 7))
 
-     // display(a_view, "parent matrix");
-
-   // s00
-      sub_matrix_view<float,2,2,0,0> s00{a_view};
-    //  display(s00, "s00");
-
-      QUAN_CHECK((s00.at(0,0) == 4))
-      QUAN_CHECK((s00.at(0,1) == 7))
-
-      QUAN_CHECK((s00.at(1,0) == 1))
-      QUAN_CHECK((s00.at(1,1) == 5))
-
+      QUAN_CHECK((s00.at<1,0>() == 1))
+      QUAN_CHECK((s00.at<1,1>() == 5))
+    
    // s01
-      sub_matrix_view<float,2,2,0,1> s01{a_view};
-     // display(s01, "s01");
+      auto const s01 =quan::fusion::make_sub_matrix_view<0,1> (m);
 
-      QUAN_CHECK((s01.at(0,0) == 2))
-      QUAN_CHECK((s01.at(0,1) == 7))
+      QUAN_CHECK((s01.at<0,0>() == 2))
+      QUAN_CHECK((s01.at<0,1>() == 7))
 
-      QUAN_CHECK((s01.at(1,0) == -3))
-      QUAN_CHECK((s01.at(1,1) == 5))
+      QUAN_CHECK((s01.at<1,0>() == -3))
+      QUAN_CHECK((s01.at<1,1>() == 5))
 
    // s02
-      sub_matrix_view<float,2,2,0,2> s02{a_view};
-     // display(s02, "s02");
+      auto const s02 = quan::fusion::make_sub_matrix_view<0,2>(m);
 
-      QUAN_CHECK((s02.at(0,0) == 2))
-      QUAN_CHECK((s02.at(0,1) == 4))
+      QUAN_CHECK((s02.at<0,0>() == 2))
+      QUAN_CHECK((s02.at<0,1>() == 4))
 
-      QUAN_CHECK((s02.at(1,0) == -3))
-      QUAN_CHECK((s02.at(1,1) == 1))
+      QUAN_CHECK((s02.at<1,0>() == -3))
+      QUAN_CHECK((s02.at<1,1>() == 1))
 
    // s10
-      sub_matrix_view<float,2,2,1,0> s10{a_view};
+      auto const s10 = quan::fusion::make_sub_matrix_view<1,0>(m);
 
-      QUAN_CHECK((s10.at(0,0) == 90))
-      QUAN_CHECK((s10.at(0,1) == 72))
+      QUAN_CHECK((s10.at<0,0>() == 90))
+      QUAN_CHECK((s10.at<0,1>() == 72))
 
-      QUAN_CHECK((s10.at(1,0) == 1))
-      QUAN_CHECK((s10.at(1,1) == 5))
+      QUAN_CHECK((s10.at<1,0>() == 1))
+      QUAN_CHECK((s10.at<1,1>() == 5))
 
     // s11
-      sub_matrix_view<float,2,2,1,1> s11{a_view};
+      auto const s11 = quan::fusion::make_sub_matrix_view<1,1>(m);
 
-      QUAN_CHECK((s11.at(0,0) == 56))
-      QUAN_CHECK((s11.at(0,1) == 72))
+      QUAN_CHECK((s11.at<0,0>() == 56))
+      QUAN_CHECK((s11.at<0,1>() == 72))
 
-      QUAN_CHECK((s11.at(1,0) == -3))
-      QUAN_CHECK((s11.at(1,1) == 5))
+      QUAN_CHECK((s11.at<1,0>() == -3))
+      QUAN_CHECK((s11.at<1,1>() == 5))
 
     // s12
-      sub_matrix_view<float,2,2,1,2> s12{a_view};
+      auto const s12 = quan::fusion::make_sub_matrix_view<1,2>(m);
 
-      QUAN_CHECK((s12.at(0,0) == 56))
-      QUAN_CHECK((s12.at(0,1) == 90))
+      QUAN_CHECK((s12.at<0,0>() == 56))
+      QUAN_CHECK((s12.at<0,1>() == 90))
 
-      QUAN_CHECK((s12.at(1,0) == -3))
-      QUAN_CHECK((s12.at(1,1) == 1))
+      QUAN_CHECK((s12.at<1,0>() == -3))
+      QUAN_CHECK((s12.at<1,1>() == 1))
 
     // s20
-      sub_matrix_view<float,2,2,2,0> s20{a_view};
+      auto const s20 = quan::fusion::make_sub_matrix_view<2,0>(m);
 
-      QUAN_CHECK((s20.at(0,0) == 90))
-      QUAN_CHECK((s20.at(0,1) == 72))
+      QUAN_CHECK((s20.at<0,0>() == 90))
+      QUAN_CHECK((s20.at<0,1>() == 72))
 
-      QUAN_CHECK((s20.at(1,0) == 4))
-      QUAN_CHECK((s20.at(1,1) == 7))
+      QUAN_CHECK((s20.at<1,0>() == 4))
+      QUAN_CHECK((s20.at<1,1>() == 7))
 
    //s21
-      sub_matrix_view<float,2,2,2,1> s21{a_view};
+      auto const s21 = quan::fusion::make_sub_matrix_view<2,1>(m);
 
-      QUAN_CHECK((s21.at(0,0) == 56))
-      QUAN_CHECK((s21.at(0,1) == 72))
+      QUAN_CHECK((s21.at<0,0>() == 56))
+      QUAN_CHECK((s21.at<0,1>() == 72))
 
-      QUAN_CHECK((s21.at(1,0) == 2))
-      QUAN_CHECK((s21.at(1,1) == 7))
+      QUAN_CHECK((s21.at<1,0>() == 2))
+      QUAN_CHECK((s21.at<1,1>() == 7))
 
    //s22
-      sub_matrix_view<float,2,2,2,2> s22{a_view};
+      auto const s22 = quan::fusion::make_sub_matrix_view<2,2>(m);
 
-      QUAN_CHECK((s22.at(0,0) == 56))
-      QUAN_CHECK((s22.at(0,1) == 90))
+      QUAN_CHECK((s22.at<0,0>() == 56))
+      QUAN_CHECK((s22.at<0,1>() == 90))
 
-      QUAN_CHECK((s22.at(1,0) == 2))
-      QUAN_CHECK((s22.at(1,1) == 4))
-*/
+      QUAN_CHECK((s22.at<1,0>() == 2))
+      QUAN_CHECK((s22.at<1,1>() == 4))
+
    }
 
 
