916c3eb4c0b3e96e7794eaccaffae403a92d9eb8
Andy Little <kwikius@yahoo.com>
2018-02-03 22:24:08 +0000

    fun::at_matrix added to master

---
+++ quan/fun/at_matrix.hpp
@@ -0,0 +1,53 @@
+#ifndef QUAN_FUN_AT_MATRIX_HPP_INCLUDED
+#define QUAN_FUN_AT_MATRIX_HPP_INCLUDED
+
+#include <quan/fun/access_modifiers.hpp>
+#include <quan/where.hpp>
+#include <quan/fun/is_fun_matrix.hpp>
+#include <quan/meta/bool.hpp>
+
+namespace quan{ namespace fun{
+   
+    namespace impl{
+       template<int R, int C, typename Matrix, typename Access, typename Where = void>
+       struct at_matrix_impl;
+
+       template<int R, int C, typename Matrix, typename Access>
+       struct at_matrix_impl<R,C,Matrix, Access,
+         typename quan::where_<
+            quan::meta::and_<
+               quan::fun::is_fun_matrix<Matrix>
+               ,quan::meta::bool_< (R < matrix_row_size<Matrix>::value) >
+               ,quan::meta::bool_< (C < matrix_col_size<Matrix>::value) >
+            >
+         >::type
+       >{
+
+           typedef typename Matrix::template type_at<R,C,Access>::type type;
+         //  typedef typename Access::template result<raw_type>::type type;
+
+           constexpr type operator()( Matrix & m)
+           {
+               return m. template at<R,C>();
+           }
+
+           constexpr type operator()( Matrix const & m)
+           {
+               return m. template at<R,C>();
+           }
+       };
+    }
+
+    template<int R,int C, typename Matrix, typename Access = as_value>
+    struct at_matrix : impl::at_matrix_impl<
+      R, C,
+      typename std::remove_const<
+         typename std::remove_reference<Matrix>::type
+      >::type,
+      Access
+    >{};
+}};
+
+
+
+#endif // QUAN_FUN_AT_MATRIX_HPP_INCLUDED
