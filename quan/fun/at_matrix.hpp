ba217904246e15867744daa04112ee1e7ea913aa
Andy Little <kwikius@yahoo.com>
2018-02-04 20:55:11 +0000

    fun.at_matrix : split into separate specialisations for constness

--- quan/fun/at_matrix.hpp
+++ quan/fun/at_matrix.hpp
@@ -9,11 +9,49 @@
 namespace quan{ namespace fun{
    
     namespace impl{
+
        template<int R, int C, typename Matrix, typename Access, typename Where = void>
        struct at_matrix_impl;
 
-       template<int R, int C, typename Matrix, typename Access>
-       struct at_matrix_impl<R,C,Matrix, Access,
+       template<int R, int C, typename Matrix>
+       struct at_matrix_impl<R,C,Matrix, as_ref,
+         typename quan::where_<
+            quan::meta::and_<
+               quan::fun::is_fun_matrix<Matrix>
+               ,quan::meta::bool_< (R < matrix_row_size<Matrix>::value) >
+               ,quan::meta::bool_< (C < matrix_col_size<Matrix>::value) >
+            >
+         >::type
+       >{
+           typedef typename Matrix::template type_at<R,C,as_ref>::type type;
+
+           constexpr type operator()( Matrix & m)
+           {
+               return m. template at<R,C>();
+           }
+       };
+
+       template<int R, int C, typename Matrix>
+       struct at_matrix_impl<R,C,Matrix, as_const_ref,
+         typename quan::where_<
+            quan::meta::and_<
+               quan::fun::is_fun_matrix<Matrix>
+               ,quan::meta::bool_< (R < matrix_row_size<Matrix>::value) >
+               ,quan::meta::bool_< (C < matrix_col_size<Matrix>::value) >
+            >
+         >::type
+       >{
+
+           typedef typename Matrix::template type_at<R,C,as_const_ref>::type type;
+
+           constexpr type operator()( Matrix const & m)
+           {
+               return m. template at<R,C>();
+           }
+       };
+
+       template<int R, int C, typename Matrix>
+       struct at_matrix_impl<R,C,Matrix, as_value,
          typename quan::where_<
             quan::meta::and_<
                quan::fun::is_fun_matrix<Matrix>
@@ -24,19 +62,16 @@ namespace quan{ namespace fun{
        >{
 
            typedef typename Matrix::template type_at<R,C,as_value>::type type;
-         //  typedef typename Access::template result<raw_type>::type type;
 
-           constexpr type & operator()( Matrix & m)
-           {
-               return m. template at<R,C>();
-           }
-
-           constexpr type  operator()( Matrix const & m)
+           constexpr type operator()( Matrix const & m)
            {
                return m. template at<R,C>();
            }
        };
+
+
     }
+
 
     template<int R,int C, typename Matrix, typename Access = as_value>
     struct at_matrix : impl::at_matrix_impl<
