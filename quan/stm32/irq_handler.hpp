640d679b16b020bf71726b32c4e0b88cdc742cbf
Andy Little <kwikius@yahoo.com>
2018-03-18 09:00:51 +0000

    add stm32 irq_handler generic

---
+++ quan/stm32/irq_handler.hpp
@@ -0,0 +1,11 @@
+#ifndef QUAN_STM32_IRQ_HANDLER_HPP_INCLUDED
+#define QUAN_STM32_IRQ_HANDLER_HPP_INCLUDED
+
+namespace quan{ namespace stm32{
+
+   template <typename M>
+   void irq_handler();
+
+}}
+
+#endif // QUAN_STM32_IRQ_HANDLER_HPP_INCLUDED
