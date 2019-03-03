8f59c267e553f4d074c671343a2d8a18d0b7711c
Andy Little <kwikius@yahoo.com>
2019-03-03 19:35:04 +0000

    add s3 bit option for millis in freertos

--- quan/stm32/freertos/millis.hpp
+++ quan/stm32/freertos/millis.hpp
@@ -13,6 +13,18 @@ namespace quan{ namespace stm32{
 bear in mind its only a uint32_t really! Will overflow in approx 49 days
  could change it to a uint32_t or int32_t. then 24 days?
 */
+
+#if defined QUAN_SYSTICK_TIMER_UINT32
+
+   inline quan::time_<uint32_t>::ms millis()
+   {
+      static_assert(std::is_same<TickType_t,uint32_t>::value,"unexpected freertos ticktype");
+      return quan::time_<uint32_t>::ms{
+         static_cast<uint32_t>(xTaskGetTickCount() * (1000U/configTICK_RATE_HZ))
+      };
+   }
+
+#else
    inline quan::time_<int64_t>::ms millis()
    {
       static_assert(std::is_same<TickType_t,uint32_t>::value,"unexpected freertos ticktype");
@@ -20,6 +32,7 @@ bear in mind its only a uint32_t really! Will overflow in approx 49 days
          xTaskGetTickCount() * (1000/configTICK_RATE_HZ)
       };
    }
+#endif
 
    inline quan::time_<uint32_t>::ms millis_from_isr()
    {
