510a55301cb284dd87afc8eba9d095070a84c73c
Andy Little <kwikius@yahoo.com>
2019-11-30 22:46:48 +0000

    switch matix types for row and column matrices

--- quan/fusion/make_row_matrix.hpp
+++ quan/fusion/make_row_matrix.hpp
@@ -1,5 +1,5 @@
-#ifndef QUAN_FUSION_MAKE_COLUMN_MATRIX_HPP_INCLUDED
-#define QUAN_FUSION_MAKE_COLUMN_MATRIX_HPP_INCLUDED
+#ifndef QUAN_FUSION_MAKE_ROW_MATRIX_HPP_INCLUDED
+#define QUAN_FUSION_MAKE_ROW_MATRIX_HPP_INCLUDED
 
 #include <quan/fusion/make_matrix.hpp>
 #include <quan/fusion/matrix.hpp>
@@ -10,18 +10,18 @@ namespace quan { namespace fusion {
 
    namespace detail{
 
-      struct make_3d_column_matrix_impl{
+      struct make_3d_row_matrix_impl{
 
          template <typename Q>
          struct apply {
             typedef quan::fun::vector<Q,Q,Q,int> vect_type;
-            typedef quan::fun::matrix<4,1,vect_type > type;
+            typedef quan::fun::matrix<1,4,vect_type > type;
          };
          template <typename Q>
          typename apply<Q>::type
          operator()( quan::three_d::vect<Q> const & in) const
          {
-            return quan::fusion::make_matrix<4>(in.x, in.y, in.z, 1);
+            return quan::fusion::make_matrix<1>(in.x, in.y, in.z, 1);
          }
 
       };
@@ -30,13 +30,13 @@ namespace quan { namespace fusion {
 
    template <typename Q>
    inline
-   typename quan::fusion::detail::make_3d_column_matrix_impl::apply<Q>::type
-   make_column_matrix(quan::three_d::vect<Q> const & in)
+   typename quan::fusion::detail::make_3d_row_matrix_impl::apply<Q>::type
+   make_row_matrix(quan::three_d::vect<Q> const & in)
    {
-      return quan::fusion::detail::make_3d_column_matrix_impl{} (in);
+      return quan::fusion::detail::make_3d_row_matrix_impl{} (in);
    }
 
 }}
 
 
-#endif // QUAN_FUSION_MAKE_COLUMN_MATRIX_HPP_INCLUDED
+#endif // QUAN_FUSION_MAKE_ROW_MATRIX_HPP_INCLUDED
