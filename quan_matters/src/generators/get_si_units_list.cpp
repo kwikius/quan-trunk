4c501be9f1d533713b7b4c468dcf90e52ef42908
Andy Little <kwikius@yahoo.com>
2019-12-04 11:50:42 +0000

    useful si_units generator

---
+++ quan_matters/src/generators/get_si_units_list.cpp
@@ -0,0 +1,33 @@
+
+
+
+#include <quan/meta/si_units.hpp>
+#include <quan/fusion/make_vector.hpp>
+
+auto get_si_units_list()
+{
+  using quan::meta::si_unit;
+  return quan::fusion::make_vector(
+         si_unit::yotta{},
+         si_unit::zetta{},
+         si_unit::exa{},
+         si_unit::peta{},
+         si_unit::tera{},
+         si_unit::giga{},
+         si_unit::mega{},
+         si_unit::kilo{},
+         si_unit::hecto{},
+         si_unit::deka{},
+         si_unit::none{},
+         si_unit::deci{},
+         si_unit::centi{},
+         si_unit::milli{},
+         si_unit::micro{},
+         si_unit::nano{},
+         si_unit::pico{},
+         si_unit::femto{},
+         si_unit::atto{},
+         si_unit::zepto{},
+         si_unit::yocto{}
+      ); 
+}
