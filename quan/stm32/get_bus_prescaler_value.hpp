9f677fadf36c2a50341ae79ac989ae8910e73380
Andy Little <kwikius@yahoo.com>
2019-12-05 09:57:18 +0000

    ws only

--- quan/stm32/get_bus_prescaler_value.hpp
+++ quan/stm32/get_bus_prescaler_value.hpp
@@ -4,79 +4,81 @@
 #include <quan/stm32/config.hpp>
 #include <quan/stm32/detail/buses.hpp>
 
-namespace quan{ namespace stm32{ namespace detail{
+namespace quan{ namespace stm32{ 
+   
+   namespace detail{
 
-   template <typename AxB_Bus>
-   struct get_bus_prescaler_value;
+      template <typename AxB_Bus>
+      struct get_bus_prescaler_value;
 
-#if defined QUAN_STM32F0
+   #if defined QUAN_STM32F0
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb1>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb1>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-  template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb2>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   };
+     template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb2>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::apb>{
-      static constexpr uint32_t value = QUAN_STM32_APB_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::apb>{
+         static constexpr uint32_t value = QUAN_STM32_APB_DIVISOR ;
+      };
 
-#elif defined QUAN_STM32F4
+   #elif defined QUAN_STM32F4
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb1>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb1>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb2>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   }
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb2>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb3>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb3>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::apb1>{
-      static constexpr uint32_t value = QUAN_STM32_APB1_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::apb1>{
+         static constexpr uint32_t value = QUAN_STM32_APB1_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::apb2>{
-      static constexpr uint32_t value = QUAN_STM32_APB2_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::apb2>{
+         static constexpr uint32_t value = QUAN_STM32_APB2_DIVISOR ;
+      };
 
-#elif defined QUAN_STM32L4
+   #elif defined QUAN_STM32L4
 
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb1>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb1>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::ahb2>{
-      static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::ahb2>{
+         static constexpr uint32_t value = QUAN_STM32_AHB_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::apb1>{
-      static constexpr uint32_t value = QUAN_STM32_APB1_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::apb1>{
+         static constexpr uint32_t value = QUAN_STM32_APB1_DIVISOR ;
+      };
 
-   template <> 
-   struct get_bus_prescaler_value<quan::stm32::detail::apb2>{
-      static constexpr uint32_t value = QUAN_STM32_APB2_DIVISOR ;
-   };
+      template <> 
+      struct get_bus_prescaler_value<quan::stm32::detail::apb2>{
+         static constexpr uint32_t value = QUAN_STM32_APB2_DIVISOR ;
+      };
 
-#endif
+   #endif
 
    }// detail
 
