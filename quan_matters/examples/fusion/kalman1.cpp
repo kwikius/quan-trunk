fdbdea42dd34a8e0bd6b9200ee9c6a7b7ad8e1e8
Andy Little <kwikius@yahoo.com>
2021-06-18 16:46:44 +0100

    update kalman example

--- quan_matters/examples/fusion/kalman1.cpp
+++ quan_matters/examples/fusion/kalman1.cpp
@@ -9,7 +9,13 @@ http://www.cl.cam.ac.uk/~rmf25/papers/Understanding%20the%20Basis%20of%20the%20K
 
 */
 
+#if __cplusplus < 200000L
+#error requires C++20
+#endif
+#if ! defined __MBED__
+
 #include <quan/fusion/make_matrix.hpp>
+#include <quan/fusion/static_value.hpp>
 
 #include <quan/fun/display_matrix.hpp>
 #include <quan/fusion/matrix.hpp>
@@ -30,44 +36,80 @@ namespace {
   QUAN_QUANTITY_LITERAL(reciprocal_length,per_m)
   QUAN_QUANTITY_LITERAL(reciprocal_time,per_ms)
   QUAN_QUANTITY_LITERAL(acceleration,m_per_s2)
+
+
+  template <typename T,int V>
+  constexpr auto static_ = quan::fusion::static_value<T,quan::meta::rational<V> >{};
 }
 
 int kalman1() 
 {
-    quan::three_d::vect<quan::length::m>              p{0.0_m,0.0_m,0.0_m};
-    quan::three_d::vect<quan::velocity::m_per_s>      v{1.0_m_per_s,0_m_per_s,1_m_per_s};
-    quan::three_d::vect<quan::acceleration::m_per_s2> a{0.0_m_per_s2,0.1_m_per_s2,0_m_per_s2};
+    quan::three_d::vect<quan::length::m>  constexpr             s{1.0_m,1.0_m,1.0_m};
+    quan::three_d::vect<quan::velocity::m_per_s> constexpr      u{0.0_m_per_s,0_m_per_s,0_m_per_s};
+    quan::three_d::vect<quan::acceleration::m_per_s2> constexpr a{0.0_m_per_s2,0.0_m_per_s2,-0.0_m_per_s2};
 
-    auto constexpr dt = 1.0_s;
-#if defined __MBED__
-    auto const m1 = quan::fusion::make_matrix<3>
-#else
+    auto constexpr dt = static_<quan::time::s,1>;
+    auto constexpr _0 = static_<int,0>;
+    auto constexpr _1 = static_<int,1>;
+    auto constexpr _2 = static_<int,2>;
+
+    /*
+      matrix is upper_triangular
+     if (r > c) then type = reciprocal type at c,r
+     and numeric_value is 0
+    (0,0),(0,1),(0,2)
+          (1,1),(1,2)
+                 (2,2)
+     (1,0)
+     (2,0) (2,1)
+    */
     auto constexpr m1 = quan::fusion::make_matrix<3>
-#endif
     (
-       1.0                  ,      dt  ,  quan::pow<2>(dt)/2.0,
-       0.0/dt               ,     1.0  ,                dt  ,
-       0.0/quan::pow<2>(dt) ,     0.0/dt ,               1.0
+       // s <-         s * 1 +  u * dt  + a *dt^2/2
+                          _1 ,      dt  , (dt*dt)/_2,
+
+       // u <- s *      0/dt +  u * 1   +  a * dt
+                       _0/dt ,     _1   ,      dt,
+
+       // a <-    s * 0/dt^2 + u * 0/dt +   a * 1
+                  _0/(dt*dt) ,    _0/dt ,      _1
     );
 
-    // ds = u * dt + 1/2 a*dt^2 :  s <- old_s + u * dt + 1/2 a*dt^2  // p * 1    + v * dt +  a * dt^2 / 2
-    // v = u + a * dt           :  v <- v + a * dt                   // p * 0/dt + v * 1  +  a * dt
-    // a = v/dt  - u/dt         :  a <- v / dt - a                   // p * 0/dt^2 + v * dt - a * 1
-
-    auto x = quan::fusion::make_matrix<3>
-    (
-       p,
-       v,
-       a
+    // can use a matrix of vectors if each vector represents a different dimension
+    auto x = quan::fusion::make_matrix<3>(
+       s,  // distance
+       u,  // velocity
+       a   // accleration
     );
+
     display(x, "x = ");
 
-    for (int32_t i = 0; i < 3; ++i){
-       x = m1 * x;
-       display(x,"x = ");
-    }
+    auto x1 = m1 * x;
+
+    display(x1,"x1 = ");
+
+    // or put values in direct
+    auto alt_x = quan::fusion::make_matrix<3>(
+      s.x,s.y,s.z,
+      u.x,u.y,u.z,
+      a.x,a.y,a.z
+    );
+
+    auto x2 = m1 * alt_x;
+
+    display(x2, "res1 = ");
 
     return 0;
 }
+
+#if defined QUAN_STANDALONE
+int main()
+{
+   kalman1();
+}
+int errors = 0;
+#endif
+
+#endif
 
 
