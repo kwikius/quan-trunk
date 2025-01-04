02561414b9e1b182c6e68e0b721b88350589a729
Andy Little <kwikius@yahoo.com>
2025-01-04 10:33:31 +0000

    refactor water viscosity in examples

--- quan_matters/examples/reynolds_number.cpp
+++ quan_matters/examples/reynolds_number.cpp
@@ -21,6 +21,8 @@ Reynolds number
 */
 
 namespace {
+
+    // Todo interpolate density and viscosity as a function of temperature
     std::map<std::string,quan::density::kg_per_m3> density_map= {
          {"water",quan::density::kg_per_m3{1000}},
          {"air",quan::density::kg_per_m3{1.225}}
@@ -28,8 +30,19 @@ namespace {
 
     std::map<std::string,decltype(quan::pressure::Pa{} * quan::time::s{}) > viscosity_map = {
          {"water",quan::pressure::mPa{1} * quan::time::s{1} * 1.0518}, // at 18 degrees celsius
-         {"air",quan::pressure::uPa{1} * quan::time::s{1} * 18.12}
+         {"air",quan::pressure::uPa{1} * quan::time::s{1} * 18.03} // at 18 degrees celsius
     };
+
+    // These macros define quantity literals as needed
+    // https://en.cppreference.com/w/cpp/language/user_literal
+
+    QUAN_QUANTITY_LITERAL(velocity, m_per_s)
+    QUAN_QUANTITY_LITERAL(velocity, knot)
+    QUAN_QUANTITY_LITERAL(velocity, mi_per_h)
+
+    QUAN_QUANTITY_LITERAL(length, mm)
+    QUAN_QUANTITY_LITERAL(length, m)
+
 }
 
 double reynolds_number(std::string const & material,quan::length::m const & L, quan::velocity::m_per_s const & u)
@@ -44,13 +57,14 @@ int main()
 {
 #if 1
     auto  medium = "water";
-    auto  L = quan::length::m{0.025};
-    auto  u = quan::velocity::knot{1};
+    auto  L = 50.0_mm;
+    auto  u = 1.0_m_per_s;
 #else
     auto  medium = "air";
-    auto  L = quan::length::m{0.1};
-    auto  u = quan::velocity::mi_per_h{10};
+    auto  L = 0.1_m;
+    auto  u = 10.0_mi_per_h;
 #endif
-    std::cout << "At "  << L << " and " << u << " in " << medium << ", RE = " << reynolds_number(medium,L,u) << '\n';
+
+    std::cout << "At chord of " << L << " and velocity of " << u << " in " << medium << ", RE = " << reynolds_number(medium,L,u) << '\n';
 
 }
